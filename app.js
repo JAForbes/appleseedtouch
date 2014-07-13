aa869d4385a5d61abda1b9683cff741f74220e93
James <james.a.forbes@gmail.com>
2014-07-14 00:25:48 +1000

    Moved persistent state into state hash

--- app.js
+++ app.js
@@ -2,23 +2,26 @@
   
   var can = $('canvas')[0];
   var con = can.getContext('2d');
-  var i = 0;
+
   var transform = {
     pos: {
       x: can.width/2,
       y: can.height/2,
     },
     scale: 1,
+    rotation: -5,
+  }
+  var state = {
+    width: 200,
+    height: 200,
     rotation: 0,
   }
-  var width = 200;
-  var height = 200;
-  var world_rotation = -5;
+
   var mc = new Hammer.Manager(can, {
   });
 
   var pan = new Hammer.Pan();
-  mc.add(pan); // add it to the Manager instance
+  mc.add(pan);
 
   var pinch = new Hammer.Pinch();
   mc.add(pinch);
@@ -40,34 +43,33 @@
     transform.scale = ev.scale;
   });
   mc.on('pinchend',function(ev) {
-    width *= transform.scale;
-    height *= transform.scale;
+    state.width *= transform.scale;
+    state.height *= transform.scale;
     transform.scale = 1;
   })
 
   mc.on('rotatemove',function(ev){
-    transform.rotation = world_rotation + ev.rotation;
+    transform.rotation = state.rotation + ev.rotation;
   })
 
   mc.on('rotateend',function(ev){
-    world_rotation = transform.rotation;
+    state.rotation = transform.rotation;
   });
 
   mc.on('tap',_(function(ev){
     if(loaded.length>0){
       image = _(loaded).sample()
-      height = width * (image.height / image.width);
+      state.height = state.width * (image.height / image.width);
     }
   }).debounce(500,true))
 
-  var i = 0;
   var image = new Image();
   image.src = 'http://31.media.tumblr.com/7a94b337606021f0480e1bc9efe4075a/tumblr_n10rypPPNE1tq38d7o1_r1_400.jpg';
 
   setInterval(function(){
     can.width = can.width;
-    var scaledW = width*transform.scale;
-    var scaledH = height*transform.scale;
+    var scaledW = state.width*transform.scale;
+    var scaledH = state.height*transform.scale;
     con.translate(transform.pos.x,transform.pos.y);
     con.rotate(transform.rotation*Math.PI/180);
     con.drawImage(image,-scaledW/2,-scaledH/2,scaledW,scaledH)
